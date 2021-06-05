7f9ac0591dffe49e01fd6713391c54a2329d5655
Jhon Gomez <jhon.gomez@rappi.com>
2021-06-05 15:59:22 -0500

    Update index.js
    Add validation for braces and square brackets

--- index.js
+++ index.js
@@ -1,47 +1,75 @@
 
-const close = ')'
-const open = '('
-
-const parenthesisChecker = (entry) => {
-  const cleaned = [...entry].filter(letter => [open, close].includes(letter))
-  const response = findAndSubstracInternal(cleaned)
-
-  console.log((response && 'Format Error') || 'Format Ok')
-}
-
-const findAndSubstracInternal = (elements) => {
-  const indexClose = elements.indexOf(close)
-  const partial = elements.slice(0, indexClose)
-  let idnexOpen;
-
-  innerLabel:
-  for (let reverseIndex = partial.length - 1; reverseIndex >= 0; reverseIndex-- ) {
-    if (partial[reverseIndex] === open) {
-      idnexOpen = reverseIndex
-
-      break innerLabel;
+/*
+    TODO check internal elements to check if any error exist
+*/
+const cleanUp = ({
+    entry,
+    open,
+    close
+  }) => {
+    return [...entry || []].filter(letter => [open, close].includes(letter))
+  }
+  
+  const bracketsChecker = (entry) => {
+  
+    //parenthesis
+    const responseP = findAndSubstracInternal({
+      entry,
+      open: '(',
+      close: ')'
+    })
+    
+    const responseBrackets = findAndSubstracInternal({
+      entry,
+      open: '[',
+      close: ']'
+    })
+  
+    const responseBraces = findAndSubstracInternal({
+      entry,
+      open: '{',
+      close: '}'
+    })
+  
+    console.log((responseBrackets && 'Format Error Brakects') || 'Format Ok Brakects', entry)
+    console.log((responseBraces && 'Format Error Braces') || 'Format Ok Braces', entry)
+    console.log((responseP && 'Format Error Parenthesis') || 'Format Ok Parenthesis', entry)
+  }
+  
+  const findAndSubstracInternal = ({
+    entry,
+    open,
+    close
+  }) => {
+    entry = cleanUp({ entry, open, close })
+    const indexClose = entry.indexOf(close)
+    const partial = entry.slice(0, indexClose)
+    
+    let indexOpen;
+  
+    innerLabel:
+    for (let reverseIndex = partial.length - 1; reverseIndex >= 0; reverseIndex--) {
+      if (partial[reverseIndex] === open) {
+        indexOpen = reverseIndex
+  
+        break innerLabel;
+      }
+    }
+    
+    //using explicit validation because with can get a index in 0
+    if(indexOpen === undefined && entry.length) {
+      return true
+    }
+  
+    const result = entry.filter((_, index) => ![indexClose, indexOpen].includes(index))
+    
+    if (result.indexOf(close) !== -1) {
+      return findAndSubstracInternal(result)
+    } else {
+      return !!result.length
     }
   }
-
-  //using explicit validation because with can get a index in 0
-  if(idnexOpen === undefined) {
-    return false
-  }
-
-  const result = elements.filter((_, index) => ![indexClose, idnexOpen].includes(index))
   
-  if (result.indexOf(close) !== -1) {
-    return findAndSubstracInternal(result)
-  } else {
-    return !!result.length
-  }
-}
-
-console.clear()
-
-parenthesisChecker('((()))()()') //good
-parenthesisChecker('((') //bad
-parenthesisChecker('(())()') //good
-parenthesisChecker('(()') //bad
-parenthesisChecker('(a + b) * (c + 1)') //good
-parenthesisChecker('(a + b) * (c + 1 * ((g + 1) / 2))') //good
+  console.clear()
+  
+  bracketsChecker('{[}') //bad
