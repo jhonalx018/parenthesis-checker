ea7939acf50832081b78e38306b1f3997381dea9
Jhon Gomez <jhon.gomez@rappi.com>
2021-06-06 10:48:12 -0500

    Update index.js
    check nested brackets

--- index.js
+++ index.js
@@ -1,75 +1,41 @@
 
-/*
-    TODO check internal elements to check if any error exist
-*/
-const cleanUp = ({
-    entry,
-    open,
-    close
-  }) => {
-    return [...entry || []].filter(letter => [open, close].includes(letter))
-  }
-  
-  const bracketsChecker = (entry) => {
-  
-    //parenthesis
+const bracketsChecker = (entry) => {
+    //brackets
     const responseP = findAndSubstracInternal({
       entry,
-      open: '(',
-      close: ')'
     })
+  
     
-    const responseBrackets = findAndSubstracInternal({
-      entry,
-      open: '[',
-      close: ']'
-    })
-  
-    const responseBraces = findAndSubstracInternal({
-      entry,
-      open: '{',
-      close: '}'
-    })
-  
-    console.log((responseBrackets && 'Format Error Brakects') || 'Format Ok Brakects', entry)
-    console.log((responseBraces && 'Format Error Braces') || 'Format Ok Braces', entry)
-    console.log((responseP && 'Format Error Parenthesis') || 'Format Ok Parenthesis', entry)
+    console.log((responseP && 'Format Error brackets') || 'Format Ok brackets', entry)
   }
   
   const findAndSubstracInternal = ({
     entry,
-    open,
-    close
   }) => {
-    entry = cleanUp({ entry, open, close })
-    const indexClose = entry.indexOf(close)
-    const partial = entry.slice(0, indexClose)
-    
-    let indexOpen;
+    const braces = {
+      '[': ']',
+      '(': ')',
+      '{': '}',
+    }
   
-    innerLabel:
-    for (let reverseIndex = partial.length - 1; reverseIndex >= 0; reverseIndex--) {
-      if (partial[reverseIndex] === open) {
-        indexOpen = reverseIndex
+    const stack = [];
   
-        break innerLabel;
+    [...entry || ''].filter((letter) => {
+      return Object.entries(braces).some((brace) => brace.includes(letter))
+    }).forEach((brace) => {
+      //open braces
+      const open = Object.keys(braces)
+      
+      if (open.includes(brace)) {
+        stack.push(brace)
+      } else if (braces[stack[stack.length - 1]] === brace) {
+        stack.pop(brace)
       }
-    }
-    
-    //using explicit validation because with can get a index in 0
-    if(indexOpen === undefined && entry.length) {
-      return true
-    }
+    })
   
-    const result = entry.filter((_, index) => ![indexClose, indexOpen].includes(index))
-    
-    if (result.indexOf(close) !== -1) {
-      return findAndSubstracInternal(result)
-    } else {
-      return !!result.length
-    }
+    return stack.length
   }
   
   console.clear()
-  
-  bracketsChecker('{[}') //bad
+  bracketsChecker('{[(])}') //bad
+  bracketsChecker('{[{}]}') //god
